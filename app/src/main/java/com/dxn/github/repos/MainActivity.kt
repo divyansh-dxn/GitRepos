c4e8cfc254bb8f0d40034f367284111ab8004201
Divyansh Kushwaha <kushwaha.divyansh.dxn@gmail.com>
2022-04-29 19:34:20 +0530

    first commit

---
+++ app/src/main/java/com/dxn/github/repos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dxn.github.repos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
